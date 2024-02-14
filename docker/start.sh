6e954347390c39bf3547c145e2b2b7d9e7be9f9c
Benedith Mulongo <benedith@sunet.se>
2024-02-14 12:48:57 +0100

    small change for testing saml

--- docker/start.sh
+++ docker/start.sh
@@ -20,7 +20,10 @@ workers=${workers-2}
 worker_class=${worker_class-sync}
 worker_threads=${worker_threads-4}
 worker_timeout=${worker_timeout-30}
-gunicorn_args="--bind 0.0.0.0:8080 --worker-tmp-dir /dev/shm -w ${workers} -k ${worker_class} --threads ${worker_threads} -t ${worker_timeout} niweb.wsgi"
+# gunicorn_args="--bind 0.0.0.0:8080 --worker-tmp-dir /dev/shm -w ${workers} -k ${worker_class} --threads ${worker_threads} -t ${worker_timeout} niweb.wsgi"
+### ADDED-----
+gunicorn_args="--bind 0.0.0.0:8080 --capture-output --access-logfile /var/log/ni/gunicorn.access.log --error-logfile /var/log/ni/gunicorn.error.log --worker-tmp-dir /dev/shm -w ${workers} -k ${worker_class} --threads ${worker_threads} -t ${worker_timeout} niweb.wsgi"
+### ADDED-----
 
 # set PYTHONPATH if it is not already set using Docker environment
 export PYTHONPATH=${PYTHONPATH-${project_dir}}
@@ -42,6 +45,13 @@ export DJANGO_SETTINGS_MODULE=${django_settings_module}
 
 mkdir -p "${log_dir}" "${state_dir}" "${media_dir}" "${cache_dir}" "${backup_dir}"
 chown -R ni: "${log_dir}" "${state_dir}" "${media_dir}" "${cache_dir}" "${backup_dir}"
+
+### ADDED-----
+touch /var/log/ni/gunicorn.access.log
+touch /var/log/ni/gunicorn.error.log
+chmod 764 /var/log/ni/gunicorn.access.log
+chmod 764 /var/log/ni/gunicorn.error.log
+### ADDED-----
 
 start-stop-daemon --start -c ni:ni --exec \
      /opt/ni/env/bin/gunicorn \
